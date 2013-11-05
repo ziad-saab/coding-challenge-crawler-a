3828eadd3f88acb7beab4d098ce7049c2a307a36
lrhache <github@lrhache.com>
2013-11-05 10:04:27 -0500

    PEP format / datetime import failure

--- run.py
+++ run.py
@@ -1,42 +1,45 @@
-#!/usr/local/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8
-
-# Requires Python 2.7
 import argparse
 import datetime
 
-##
-# Parse a date string
-#
-# @param s the string representation of the date, using the YYYY-MM-DD format
-#
+
 def parse_date(s):
-    return datetime.strptime(s, '%Y-%m-%d')
+    """Parse a date string
 
-##
-# Get today's date, with an optional offset
-#
-# @param offset the number of days to offset from today
-#
+    Arguments:
+    s -- Date as string in format: YYYY-MM-DD
+    """
+    return datetime.datetime.strptime(s, '%Y-%m-%d')
+
+
 def today(offset=0):
-    return datetime.date.today() + datetime.timedelta(offset)
+    """Get today's date, with an optional offset
 
-parser = argparse.ArgumentParser(description='Crawl web site.')
-parser.add_argument('--extract',   required=True,  choices=['stops', 'departures'], help='the type of extraction to perform')
-parser.add_argument('--output',    required=True,  type=argparse.FileType('w'),     help='the path of the output file to generate')
-parser.add_argument('--startdate', required=False, type=parse_date, default=today(), help='the beginning of the departure date range')
-parser.add_argument('--enddate',   required=False, type=parse_date, default=today(7), help='the end of the departure date range')
+    Keyword arguments:
+    offset -- offset the number of days to offset from today (default 0)
+    """
+    return datetime.date.today() + datetime.timedelta(days=offset)
 
-args = parser.parse_args()
 
-if args.extract == 'stops':
-    print 'downloading stops to %s' % args.output
-    exit(0)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Crawl web site.')
+    parser.add_argument('--extract', required=True, choices=['stops', 'departures'],
+                        help='the type of extraction to perform')
+    parser.add_argument('--output', required=True, type=argparse.FileType('w'),
+                        help='the path of the output file to generate')
+    parser.add_argument('--startdate', required=False, type=parse_date, default=today(),
+                        help='the beginning of the departure date range')
+    parser.add_argument('--enddate', required=False, type=parse_date, default=today(7),
+                        help='the end of the departure date range')
 
-elif args.extract == 'departures':
-    print 'downloading departures to %s for dates %s through %s' % (args.output, args.startdate, args.enddate)
-    exit(0)
+    args = parser.parse_args()
 
-else:
-    print 'invalid arguments, check inputs'
-    exit(-1)
+    if args.extract == 'stops':
+        print 'Downloading stops to {}'.format(args.output)
+        """ Do something"""
+
+    elif args.extract == 'departures':
+        print 'Downloading departures to {0} for dates {1} through {2}'.format(
+            args.output, args.startdate, args.enddate)
+        """ Do something"""
